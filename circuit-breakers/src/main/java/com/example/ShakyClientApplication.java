caa1acae92b5e6dc52b648616396b8dee4e60be6
Josh Long <josh@joshlong.com>
2016-06-12 00:05:11 +0300

    edge services

---
+++ circuit-breakers/src/main/java/com/example/ShakyClientApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShakyClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShakyClientApplication.class, args);
+	}
+}
