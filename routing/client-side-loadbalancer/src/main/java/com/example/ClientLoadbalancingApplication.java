7515ba15b2c72794c48dd9345bb674b6f1d4fe8e
Josh Long <josh@joshlong.com>
2016-07-15 18:49:57 -0400

    cslb

--- routing/client-side-loadbalancer/src/main/java/com/example/ClientLoadbalancingApplication.java
+++ routing/client-side-loadbalancer/src/main/java/com/example/ClientLoadbalancingApplication.java
@@ -2,7 +2,9 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ClientLoadbalancingApplication {
 
@@ -10,3 +12,4 @@ public class ClientLoadbalancingApplication {
 		SpringApplication.run(ClientLoadbalancingApplication.class, args);
 	}
 }
+
