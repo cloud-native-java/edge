7efa7019ebd88128e2899db3785f339daadba928
Josh Long <josh@joshlong.com>
2016-09-05 13:41:07 +0800

    moar

--- auth-service/src/main/java/auth/AuthServiceApplication.java
+++ auth-service/src/main/java/auth/AuthServiceApplication.java
@@ -1,9 +1,28 @@
 package auth;
 
+import auth.accounts.Account;
+import auth.accounts.AccountRepository;
+import auth.clients.Client;
+import auth.clients.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+import java.util.stream.Stream;
 
 
 // TODO http://blog.jdriven.com/2016/09/securing-application-landscape-spring-cloud-security-part-1/
@@ -16,4 +35,68 @@ public class AuthServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthServiceApplication.class, args);
     }
+}
+
+@Configuration
+@EnableAuthorizationServer
+class AuthorizationServer extends AuthorizationServerConfigurerAdapter {
+
+    private final AuthenticationManager authenticationManager;
+
+    private final ClientDetailsService clientDetailsService;
+
+    @Autowired
+    public AuthorizationServer(AuthenticationManager authenticationManager,
+                               ClientDetailsService clientDetailsService) {
+        this.authenticationManager = authenticationManager;
+        this.clientDetailsService = clientDetailsService;
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.withClientDetails(clientDetailsService);
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager);
+    }
+}
+
+
+@Component
+class DataCommandLineRunner implements CommandLineRunner {
+
+    private final AccountRepository accountRepository;
+
+    private final ClientRepository clientRepository;
+
+    @Autowired
+    public DataCommandLineRunner(AccountRepository accountRepository, ClientRepository clientRepository) {
+        this.accountRepository = accountRepository;
+        this.clientRepository = clientRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Stream.of("dsyer,cloud", "pwebb,boot",
+                "mminella,batch", "rwinch,security", "jlong,spring")
+                .map(s -> s.split(","))
+                .forEach(tuple -> accountRepository.save(new Account(tuple[0], tuple[1], true)));
+
+        Stream.of("acme,acmesecret")
+                .map(x -> x.split(","))
+                .forEach(x -> clientRepository.save(new Client(x[0], x[1])));
+    }
+}
+
+@RestController
+class PrincipalRestController {
+
+    @RequestMapping("/user")
+    Principal principal(Principal principal) {
+        return principal;
+    }
+
 }
--- sso-client/src/main/java/com/example/SsoApplication.java
+++ sso-client/src/main/java/com/example/SsoApplication.java
@@ -1,44 +1,48 @@
+/*
+ * Copyright 2012-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example;
+
+import java.security.Principal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.csrf.CsrfFilter;
-import org.springframework.security.web.csrf.CsrfToken;
-import org.springframework.security.web.csrf.CsrfTokenRepository;
-import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.filter.OncePerRequestFilter;
-import org.springframework.web.util.WebUtils;
-
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.security.Principal;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @EnableOAuth2Sso
+@RestController
 public class SsoApplication extends WebSecurityConfigurerAdapter {
 
-//    @RequestMapping("/user")
-//    public Principal user(Principal principal) {
-//        return principal;
-//    }
+    @RequestMapping("/user")
+    public Principal user(Principal principal) {
+        return principal;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        // @formatter:off
         http.antMatcher("/**").authorizeRequests().antMatchers("/", "/login**", "/webjars/**").permitAll().anyRequest()
-                .authenticated();
+                .authenticated().and().logout().logoutSuccessUrl("/").permitAll().and().csrf()
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+        // @formatter:on
     }
 
     public static void main(String[] args) {
