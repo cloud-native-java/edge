99dd5a6f20b751fd5f9335f06736e1bf11dbfde7
Josh Long <josh@joshlong.com>
2016-09-12 00:03:04 +0800

    edge services and microproxies oh my!

--- zuul-microproxy/src/main/java/edge/ZuulMicroproxyApplication.java
+++ zuul-microproxy/src/main/java/edge/ZuulMicroproxyApplication.java
@@ -4,57 +4,63 @@ import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 import org.springframework.cloud.netflix.zuul.filters.RouteLocator;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
+import java.util.Collections;
+import java.util.Map;
 
+@EnableDiscoveryClient
 @EnableZuulProxy // <1>
 @SpringBootApplication
 public class ZuulMicroproxyApplication {
 
     @Bean
     CommandLineRunner commandLineRunner(RouteLocator routeLocator) {
-        return args -> routeLocator.getRoutes().forEach(r -> {
-            System.out.println(r.toString());
-        });
+        return args -> routeLocator.getRoutes().forEach(r -> System.out.println(r.toString()));
+    }
+
+    @Component
+    public static class SimpleZuulFilter extends ZuulFilter {
+
+        @Override
+        public String filterType() {
+            return "pre";
+        }
+
+        @Override
+        public int filterOrder() {
+            return 0;
+        }
+
+        @Override
+        public boolean shouldFilter() {
+            return true;
+        }
+
+        @Override
+        public Object run() {
+
+            RequestContext.getCurrentContext()
+                    .getZuulRequestHeaders().forEach((k, v) -> System.out.println(k + '=' + v));
+
+            return null;
+        }
     }
 
     public static void main(String args[]) {
         SpringApplication.run(ZuulMicroproxyApplication.class, args);
-    }
-}
-
-@Component
-class DeviceDelayFilter extends ZuulFilter {
-
-    @Override
-    public String filterType() {
-        return null;
-    }
-
-    @Override
-    public int filterOrder() {
-        return 0;
-    }
-
-    @Override
-    public boolean shouldFilter() {
-        return false;
-    }
-
-    @Override
-    public Object run() {
-        RequestContext ctx = RequestContext.getCurrentContext();
-        HttpServletRequest request = ctx.getRequest();
-        ctx.getZuulRequestHeaders().forEach((k, v) -> System.out.println(k + '=' + v));
-//        log.info(String.format("%s request to %s", request.getMethod(), request.getRequestURL().toString()));
-
-        return null;
-
     }
 }
 
