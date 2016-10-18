2c95f69bceeb42f8a1571e11e397f7984a6e7225
Josh Long <josh@joshlong.com>
2016-10-17 23:27:34 -0700

    adding information related to a csutom Zuul filter

--- html5-client/src/main/java/client/Html5Client.java
+++ html5-client/src/main/java/client/Html5Client.java
@@ -3,7 +3,6 @@ package client;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.http.MediaType;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
-import java.util.List;
 import java.util.Map;
 
 @EnableDiscoveryClient
@@ -27,20 +25,20 @@ public class Html5Client {
         this.discoveryClient = discoveryClient;
     }
 
+    public static void main(String[] args) {
+        SpringApplication.run(Html5Client.class, args);
+    }
+
     // <1>
     @RequestMapping(value = "/greetings-client-uri",
             method = RequestMethod.GET,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    Map<String, String> greetingClientURI() throws Exception {
+    Map<String, String> greetingsClientURI() throws Exception {
         return discoveryClient.getInstances("greetings-client")
                 .stream()
                 .findAny()
                 .map(serviceInstance -> Collections.singletonMap("uri", serviceInstance.getUri().toString()))
                 .orElse(null);
-    }
-
-    public static void main(String[] args) {
-        SpringApplication.run(Html5Client.class, args);
     }
 }
 
