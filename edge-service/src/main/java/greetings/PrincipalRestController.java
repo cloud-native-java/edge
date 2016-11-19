c6c20bd834b0100b23b2a1a38ba904a7f73f4095
Josh Long <josh@joshlong.com>
2016-11-19 04:36:45 -0800

    factoring out the app's functionality into a separate javascript file and REST endpoint

---
+++ edge-service/src/main/java/greetings/PrincipalRestController.java
@@ -0,0 +1,17 @@
+package greetings;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@Profile("sso")
+@RestController
+class PrincipalRestController {
+
+    @RequestMapping("/user")
+    public Principal user(Principal principal) {
+        return principal;
+    }
+}
---
+++ edge-service/src/main/resources/static/app.js
@@ -0,0 +1,57 @@
+var app = angular.module("app", []);
+
+//<1>
+app.factory('oauth', function () {
+    return {details: null, name: null, token: null};
+});
+
+app.run(['$http', '$rootScope', 'oauth', function ($http, $rootScope, oauth) {
+
+    // <1>
+    $http.get("/user").success(function (data) {
+
+        oauth.details = data.userAuthentication.details;
+        oauth.name = oauth.details.name;
+        oauth.token = data.details.tokenValue;
+
+        // <2>
+        $http.defaults.headers.common['Authorization'] = 'bearer ' + oauth.token;
+
+        // <3>
+        $rootScope.$broadcast('auth-event', oauth.token);
+    });
+}]);
+
+app.controller("home", function ($http, $rootScope, oauth) {
+
+    var self = this;
+
+    self.authenticated = false;
+
+    // <4>
+    $rootScope.$on('auth-event', function (evt, ctx) {
+        self.user = oauth.details.name;
+        self.token = oauth.token;
+        self.authenticated = true;
+
+        var name = window.prompt('who would you like to greet?');
+
+        // <5>
+        $http.get('/greetings-service/greet/' + name)
+            .success(function (greetingData) {
+                self.greetingFromZuulRoute = greetingData.greeting;
+            })
+            .error(function (e) {
+                console.log('oops!' + JSON.stringify(e));
+            });
+
+        // <6>
+        $http.get('/lets/greet/' + name)
+            .success(function (greetingData) {
+                self.greetingFromEdgeService = greetingData.greeting;
+            })
+            .error(function (e) {
+                console.log('oops!' + JSON.stringify(e));
+            });
+    });
+});
