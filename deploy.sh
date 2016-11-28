f753e6ee49bece2731b7d1eed1576c7ffa869762
Josh Long <josh@joshlong.com>
2016-11-28 20:30:25 +1100

    final updated deploy.sh now correctly deploys everything including the PostgresSQL instance for the auth-service.

--- deploy.sh
+++ deploy.sh
@@ -6,32 +6,44 @@ source $BUILD_DIRECTORY/utils/cf-common.sh
 
 mvn -DskipTests clean install
 
+as=auth-service
+res=service-registry
+gs=greetings-service
+gc=edge-service
+h5=html5-client
+
+cf d -f $res
+cf d -f $as
+cf d -f $gs
+cf d -f $gc
+cf d -f $h5
 
 # EUREKA SERVICE
-res=service-registry
 cf ds -f $res
 cf a | grep $res && cf d -f $res
 deploy_app $res
 deploy_service $res
 
 # AUTH SERVICE
-as=auth-service
+as_db=auth-service-pgsql
+
 cf ds -f $as
 cf d -f $as
+cf ds -f ${as_db}
+
+cf cs elephantsql turtle ${as_db}
+
 deploy_app $as
 deploy_service $as
 
-# GREETINGS SERVICE
-gs=greetings-service
+## GREETINGS SERVICE
 cf d -f $gs
 deploy_app $gs
 
-# GREETINGS CLIENT
-gc=edge-service
+## GREETINGS CLIENT
 cf d -f $gc
 deploy_app $gc
 
-# HTML5 CLIENT
-h5=html5-client
+## HTML5 CLIENT
 cf d -f $h5
 deploy_app $h5
