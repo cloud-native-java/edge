f2c297a3690d4045193b6519ef414f105da416de
Josh Long <josh@joshlong.com>
2016-11-28 15:39:30 +1100

    adding profiled information and configuratino

---
+++ deploy.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -e
+
+source $BUILD_DIRECTORY/utils/cf-common.sh
+
+mvn -DskipTests clean install
+
+
+# EUREKA SERVICE
+#res=service-registry
+#cf ds -f $res
+#cf a | grep $res && cf d -f $res
+#deploy_app $res
+#deploy_service $res
+
+
+# AUTH SERVICE
+#as=auth-service
+#cf ds -f $as
+#cf d -f $as
+#deploy_app $as
+#deploy_service $as
+
+# GREETINGS SERVICE
+#gs=greetings-service
+#cf d -f $gs
+#deploy_app $gs
+
+# GREETINGS CLIENT
+gc=edge-service
+cf d -f $gc
+deploy_app $gc
