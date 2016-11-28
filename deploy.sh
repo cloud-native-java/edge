cfa65ff9791e8651ab9e18ef57c697e3a9e3c332
Josh Long <josh@joshlong.com>
2016-11-28 19:22:04 +1100

    lots of configuration's been added to support ease of deployment in a cloud-environment

--- deploy.sh
+++ deploy.sh
@@ -8,26 +8,30 @@ mvn -DskipTests clean install
 
 
 # EUREKA SERVICE
-#res=service-registry
-#cf ds -f $res
-#cf a | grep $res && cf d -f $res
-#deploy_app $res
-#deploy_service $res
-
+res=service-registry
+cf ds -f $res
+cf a | grep $res && cf d -f $res
+deploy_app $res
+deploy_service $res
 
 # AUTH SERVICE
-#as=auth-service
-#cf ds -f $as
-#cf d -f $as
-#deploy_app $as
-#deploy_service $as
+as=auth-service
+cf ds -f $as
+cf d -f $as
+deploy_app $as
+deploy_service $as
 
 # GREETINGS SERVICE
-#gs=greetings-service
-#cf d -f $gs
-#deploy_app $gs
+gs=greetings-service
+cf d -f $gs
+deploy_app $gs
 
 # GREETINGS CLIENT
 gc=edge-service
 cf d -f $gc
 deploy_app $gc
+
+# HTML5 CLIENT
+h5=html5-client
+cf d -f $h5
+deploy_app $h5
